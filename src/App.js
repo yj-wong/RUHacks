4b9bb8e511eb2614e0c5fe56337388b98dba142c
yj-wong <yujuewong15@gmail.com>
2019-05-18 05:04:16 -0400

    add body and navbar-collapse

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import './App.css';
 import Menu from './Navbar/Menu.js';
 import Topbar from './Navbar/Topbar.js';
+import Products from './Body/Products.js';
 
 function App() {
   return (
     <div className="App">
-    <Topbar />
-    <Menu />
+      <Topbar />
+      <Menu />
     <header className="App-header">
-
-    <p>
-    blah
-    </p>
+      <div id="body">
+        <Products />
+      </div>
     </header>
     </div>
   );
